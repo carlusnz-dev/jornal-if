05abf59d5741ec63906a81dc72eacc8caa358a89
Carlos Antunes <carlosantunes.dev@gmail.com>
2024-07-10 22:23:08 -0300

    correção na config do firebase

--- src/main/kotlin/br/com/gremiorupestre/grer/config/FirebaseConfig.kt
+++ src/main/kotlin/br/com/gremiorupestre/grer/config/FirebaseConfig.kt
@@ -3,34 +3,31 @@ package br.com.gremiorupestre.grer.config
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
-import jdk.internal.loader.Resource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ResourceLoader
-import java.io.FileInputStream
 
 @Configuration
 class FirebaseConfig {
 
     @Autowired
-    lateinit var resource: ResourceLoader
+    lateinit var resourceLoader: ResourceLoader
 
     @Value("\${firebase.private.key}")
-    lateinit var PRIVATE_KEY: String
+    lateinit var privateKey: String
 
     @Value("\${bucket.name}")
-    lateinit var STORAGE_BUCKET: String
+    lateinit var storageBucket: String
 
     @Bean
     fun initializeFirebase() {
-
-        val resource = resource.getResource(PRIVATE_KEY)
+        val resource = resourceLoader.getResource(privateKey)
 
         val options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(resource.inputStream))
-            .setStorageBucket(STORAGE_BUCKET)
+            .setStorageBucket(storageBucket)
             .build()
 
         FirebaseApp.initializeApp(options)
