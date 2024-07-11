45a90f41578dec51275d8e667a2fc8151ac5f884
Carlos Antunes <carlosantunes.dev@gmail.com>
2024-07-10 23:21:38 -0300

    alteração no estilo do article/detail e ajustes na url da imagem

--- src/main/kotlin/br/com/gremiorupestre/grer/util/FileUtil.kt
+++ src/main/kotlin/br/com/gremiorupestre/grer/util/FileUtil.kt
@@ -13,7 +13,7 @@ class FileUtil {
     private val storage: Storage = StorageOptions.getDefaultInstance().service
 
     @Value("\${bucket.name}")
-    private lateinit var bucketName: String
+    lateinit var bucketName: String
 
     companion object {
         fun create(): FileUtil = FileUtil()
